bbbf1ad27502a02ea2aafa3805647c4fdfaf58ce
KasemaaA <august.kasemaa@khk.ee>
2021-10-14 09:45:52 +0300

    muutujate deklareerimine, omistamine, kasutamine
    Signed-off-by: KasemaaA <august.kasemaa@khk.ee>

---
+++ app.js
@@ -0,0 +1,11 @@
+// muutuja andmetüübid
+// 1. deklareerimine
+// let muutujanimi
+//  väärtuse omistamine
+// muutujanimi=väärtus
+
+let = 2016
+
+let president = "Kersti Kaljulaid"
+
+console.log(aasta + " valiti Eesti presidendiks " + president)
