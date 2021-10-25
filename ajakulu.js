e0fc06c29a93ef95c18394e541362d9dae3dbe5c
KasemaaA <august.kasemaa@khk.ee>
2021-10-25 21:23:24 +0300

    Ajakulu arvutamine
    Signed-off-by: KasemaaA <august.kasemaa@khk.ee>

---
+++ ajakulu.js
@@ -0,0 +1,8 @@
+var punkt = window.prompt("Sisesta ainepunktide arv: "); //küsin ekp arvu
+var week = window.prompt( "Sisesta nädalate arv: "); // küsin nädalate arvu
+
+let ekp = (punkt*26); // arvutan ühe ekp ajakulu
+let vastus = Math.round(ekp/week) // arvutan ajakulu kokku
+
+alert("Sinu ajakulu on " + vastus + " tundi nädalas.")
+console.log(vastus) // väljastan vastuse
