0b3821b867757be44e6cdc4c1626069a7e836773
KasemaaA <august.kasemaa@khk.ee>
2021-11-12 14:05:13 +0200

    koha valimine; tingimuslause
    Signed-off-by: KasemaaA <august.kasemaa@khk.ee>

---
+++ iste.js
@@ -0,0 +1,24 @@
+
+let valik = prompt("Kas soovite valida oma istekoha? (jah/ei)");
+
+// istme valik
+if (valik === "jah") {
+    // valiti ise
+    console.log ("Valisite ise")
+    let koht = prompt("Kas soovid istet akna ääres või mujal?")
+    if(koht === "aken"){
+        console.log("Akna")
+    } else {
+        console.log("Muu koht")
+    }
+}
+else {
+    //loos
+    console.log("Istekoht loositi")
+    let toenaosus = Math.trunc(Math.ceil(Math.random()*3))
+    if(toenaosus === 1) {
+        console.log("Aknakoht")
+    } else {
+        console.log("Muu koht")
+    }
+}
