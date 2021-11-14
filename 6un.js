6d84b1c25be7b4dc1f67c6172020501a4d146b7e
KasemaaA <august.kasemaa@khk.ee>
2021-11-14 18:22:02 +0200

    tsükkel.4.õun
    Signed-off-by: KasemaaA <august.kasemaa@khk.ee>

---
+++ 6un.js
@@ -0,0 +1,14 @@
+// siesnd
+let midget = prompt("Mitu pöialpoissi tahab õunu? (0-7)")
+
+console.log( "Mitu pöialpossi tahtis õunu: " + midget)
+
+let sum_õun = 14
+
+for (let i = 1; i <= midget; i++) {
+    let õunad = Math.floor(Math.random() * 2) +1
+    sum_õun = sum_õun - õunad
+    console.log(i + ". pöialpoiss sai " + õunad)
+}
+
+console.log("Lumivalgekesele jäi alles " + sum_õun + " õuna")
